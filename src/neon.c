018c856f8d748f6001d3a44a5d62aba8ac629d88
Ariston Lorenzo <me@ariston.dev>
2023-05-19 13:21:30 +0000

    src: Added option handler to wrapper program
    * neon.c: Added option handler, moved $Id$ tag and added more comments
    
    Signed-off-by: Ariston Lorenzo <me@ariston.dev>

--- src/neon.c
+++ src/neon.c
@@ -1,8 +1,7 @@
-/*  $Id: neon.c,v 1.2 2023/05/18 14:33:02 atl Exp $  */
 /*
  * NEON - A simple project revision tracker
  *
- * Copyright (c) 2023 Ariston Lorenzo.
+ * Copyright (c) 2023 Ariston Lorenzo. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -19,8 +18,8 @@
  * contributors may be used to endorse or promote products derived from this
  * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
@@ -32,10 +31,74 @@
  * SUCH DAMAGE.
  */
 
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef lint
+static const char copyright[] =
+"@(#) Copyright (c) 2021, 2022, 2023\n\
+	Ariston Lorenzo. All rights reserved.\n";
+#endif /* not lint */
+
+#if 0
+#ifndef lint
+static char rcsid[] = "@(#) $Id: neon.c,v 1.3 2023/05/19 13:21:30 atl Exp $";
+#endif /* not lint */
+#endif
+
+/*
+ *  neon.c - the front end to the NEON revision system.
+ *
+ *  This just makes NEON easier to use by wrapping all the separate programs
+ *  together through a singular program without having to compile NEON into
+ *  one program all-together, removing the UNIX philosophy from the system.
+ *  This front-end also makes some of the names more user-friendly and closer
+ *  to other version control systems like CVS and Git.
+ *
+ *  Usage:
+ *	neon [-hv] <command> [<args>]
+ *
+ *  Flags:
+ *	-h - Prints the usage. Also works as a command argument.
+ *	-v - Prints version and copyright info.
+ *
+ *  Commands:
+ *	add  | write-cache - Adds a new file entry to the project cache.
+ *	ls   | read-cache  - Lists all the entries in the cache.
+ *	init | init-db     - Initializes the NEON filesystem/database.
+ *
+ *  Author:
+ *	Ariston Lorenzo <me@ariston.dev>
+ *	Copyright 2021, 2022, 2023 Ariston Lorenzo.
+ */
 
 int
-main(int argc, char **argv)
+main(argc, argv)
+	int argc;
+	char *argv[];
 {
+	/* variable definitions */
+	static char usage[] = "Usage: neon [-hv] <command> [<args>]\n";
+	int ch;
+
+	/* Option handling */
+	while ((ch = getopt(argc, argv, "hv")) != -1) {
+		switch (ch) {
+		case 'h':
+			printf(usage);
+			break;
+		case 'v':
+			printf("NEON version %s\n", NEON_VERSION);
+			printf("Copyright (c) 2021, 2022, 2023 Ariston Lorenzo. All rights reserved.\n");
+			break;
+		default:
+		case '?':
+			printf(usage);
+			break;
+		}
+	}
+
 	return 0;
 }
